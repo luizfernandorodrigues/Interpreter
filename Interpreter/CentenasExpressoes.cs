5ccc909665297411ca278e187fe5eee9b3acb740
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-04-09 18:32:57 -0300

    Add project files.

---
+++ Interpreter/CentenasExpressoes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    public class CentenasExpressoes : Expressao
+    {
+        public override string Cinco()
+        {
+            return "D";
+        }
+
+        public override int Multiplicar()
+        {
+            return 100;
+        }
+
+        public override string Nove()
+        {
+            return "CM";
+        }
+
+        public override string Quatro()
+        {
+            return "CD";
+        }
+
+        public override string Um()
+        {
+            return "C";
+        }
+    }
+}
---
+++ Interpreter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string romando = "MVMXXVIII";
+
+            Contexto contexto = new Contexto(romando);
+
+            List<Expressao> lista = new List<Expressao>();
+
+            lista.Add(new MilharesExpressao());
+            lista.Add(new CentenasExpressoes());
+            lista.Add(new DezenasExpressoes());
+            lista.Add(new UnidadeExpressao());
+
+            foreach (Expressao expressao in lista)
+                expressao.Interpretador(contexto);
+
+            Console.WriteLine("{0} = {1}", romando, contexto.Output);
+
+            Console.ReadKey();
+        }
+    }
+}
